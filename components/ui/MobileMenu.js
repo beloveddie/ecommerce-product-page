9d54cf4114c999846a39511952cb5fcfc104ac1a
Eddiebee <edbeeotudor@gmail.com>
2022-08-01 19:26:42 +0100

    Build up Menubar --mobile-version

--- components/ui/MobileMenu.js
+++ components/ui/MobileMenu.js
@@ -1,9 +1,18 @@
-import { Box, IconButton } from "@mui/material";
+import { useState } from "react";
+import { Box, IconButton, Drawer, Tooltip } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconMenu } from "../ui/Icons/IconMenu";
+import CloseIcon from "@mui/icons-material/Close";
 import { Logo } from "../ui/Logo";
+import { MobileNavList } from "./MobileNavList";
 
+const pages = ["Products", "Pricing", "Blog"];
 export const MobileMenu = () => {
+  const [showMenuBar, setShowMenuBar] = useState(false);
+
+  const showMenuBarHandler = () => {
+    setShowMenuBar(!showMenuBar);
+  };
+
   return (
     <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
       <IconButton
@@ -11,15 +20,35 @@ export const MobileMenu = () => {
         aria-label="account of current user"
         aria-controls="menu-appbar"
         aria-haspopup="true"
-        //   onClick={handleOpenNavMenu}
+        onClick={showMenuBarHandler}
         color="inherit"
+        sx={{ zIndex: 10 }}
       >
-        <MenuIcon />
-        {/* <IconMenu /> */}
+        {!showMenuBar ? (
+          <Tooltip title="View menu">
+            <MenuIcon />
+          </Tooltip>
+        ) : (
+          <Tooltip title="Close menu">
+            <CloseIcon />
+          </Tooltip>
+        )}
       </IconButton>
       <IconButton>
         <Logo />
       </IconButton>
+
+      {/* menu bar */}
+      {showMenuBar && (
+        <Drawer
+          anchor="left"
+          open={showMenuBar}
+          onClose={showMenuBarHandler}
+          sx={{ zIndex: 1 }}
+        >
+          <MobileNavList showMenuBarHandler={showMenuBarHandler} />
+        </Drawer>
+      )}
     </Box>
   );
 };
