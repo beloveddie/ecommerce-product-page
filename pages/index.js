07414a5fac4ec46fe5737d7454672dfa208be215
Eddiebee <edbeeotudor@gmail.com>
2022-08-01 19:29:07 +0100

    Add Header component to DOM

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import { CircularProgress } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import Header from "../components/Header/Header";
 
 export default function Home() {
   return (
     <>
-      <Typography variant="h1">eCommerce Product page</Typography>
-      <Typography variant="body1">
-        coming soon.. <CircularProgress />
-      </Typography>
-      <Button variant="outlined">Coming from MUI!</Button>
+      <Header />
     </>
   );
 }
