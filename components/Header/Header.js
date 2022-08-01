dd0b6549cf291c7be276d5d3372160fe61897821
Eddiebee <edbeeotudor@gmail.com>
2022-08-01 19:27:52 +0100

    Build up Header --mobile-version

---
+++ components/Header/Header.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Container, Box, Stack, AppBar, Toolbar } from "@mui/material/";
+import { Logo } from "../ui/Logo";
+import { MobileMenu } from "../ui/MobileMenu";
+import { IconProfile } from "../ui/Icons/IconProfile";
+import { IconCart } from "../ui/Icons/IconCart";
+
+const Header = () => {
+  return (
+    <AppBar component="header" sx={{ backgroundColor: "neutral.white" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Logo />
+          </Box>
+
+          <MobileMenu />
+
+          <Box sx={{ display: "flex" }}>
+            <Stack direction="row" spacing={2}>
+              <IconCart />
+              <IconProfile />
+            </Stack>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
