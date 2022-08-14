6c96c045ceb0238df8beb173cb121b13caa40f14
Eddiebee <edbeeotudor@gmail.com>
2022-08-14 18:55:42 +0100

    Add actual image file to ProfileIcon Component

--- components/ui/Icons/IconProfile.js
+++ components/ui/Icons/IconProfile.js
@@ -2,9 +2,9 @@ import { Tooltip, IconButton, Avatar } from "@mui/material";
 
 export const IconProfile = () => {
   return (
-    <Tooltip title="Profile Icon">
+    <Tooltip title="Esteemed, Remy Sharp">
       <IconButton sx={{ p: 0 }}>
-        <Avatar alt="Remy Sharp">G</Avatar>
+        <Avatar alt="Remy Sharp" src="/images/image-avatar.png"></Avatar>
       </IconButton>
     </Tooltip>
   );
