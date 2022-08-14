503b73cb4a0fea8820e9466209a765088079d3bf
Eddiebee <edbeeotudor@gmail.com>
2022-08-14 18:11:18 +0100

    Fix Display Bug and Finalize build of ImageSlider

--- components/ui/MobileImageSlider/MobileImageSlider.js
+++ components/ui/MobileImageSlider/MobileImageSlider.js
@@ -6,62 +6,61 @@ import NavigateBeforeRoundedIcon from "@mui/icons-material/NavigateBeforeRounded
 
 export const MobileImageSlider = () => {
   return (
-    <Box mt={4} position="relative">
-      {/* Image List */}
+    <>
       <Box
-        width={400}
-        height={400}
         sx={{
-          zIndex: -1,
           display: { lg: "none", xs: "block" },
         }}
       >
         <Image
           src="/images/image-product-1.jpg"
           alt="Product one"
-          layout="fill"
+          width={400}
+          height={400}
+          layout="responsive"
           loading="lazy"
         />
       </Box>
-      <Box
-        width="100%"
-        height={400}
-        sx={{
-          position: "absolute",
-          top: 0,
-          display: { lg: "none", xs: "flex" },
-          flexDirection: "column",
-          justifyContent: "center",
-          zIndex: 10,
-        }}
-      >
-        <Stack
+      <Box mt={4}>
+        <Box
+          width="100%"
+          height={400}
+          position="absolute"
+          top={0}
           sx={{
-            width: "100%",
-            " & button": {
-              borderRadius: "50%",
-              p: 3,
-              mx: 1,
-              width: 15,
-              height: 15,
-              backgroundColor: "neutral.white",
-            },
-            " & svg": {
-              color: "black",
-            },
+            display: { lg: "none", xs: "flex" },
+            flexDirection: "column",
+            justifyContent: "center",
           }}
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
         >
-          <IconButton variant="contained">
-            <NavigateBeforeRoundedIcon />
-          </IconButton>
-          <IconButton variant="contained">
-            <NavigateNextRoundedIcon />
-          </IconButton>
-        </Stack>
+          <Stack
+            sx={{
+              width: "100%",
+              " & button": {
+                borderRadius: "50%",
+                p: 3,
+                mx: 1,
+                width: 15,
+                height: 15,
+                backgroundColor: "neutral.white",
+              },
+              " & svg": {
+                color: "black",
+              },
+            }}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <IconButton variant="contained">
+              <NavigateBeforeRoundedIcon />
+            </IconButton>
+            <IconButton variant="contained">
+              <NavigateNextRoundedIcon />
+            </IconButton>
+          </Stack>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
