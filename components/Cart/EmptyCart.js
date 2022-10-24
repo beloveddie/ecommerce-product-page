743221efaf8b7df5761be8f52a8ce9d5fdd66ec4
Eddiebee <edbeeotudor@gmail.com>
2022-10-24 11:52:17 +0100

    Build up EmptyCart component

--- components/Cart/EmptyCart.js
+++ components/Cart/EmptyCart.js
@@ -2,9 +2,14 @@ import { ListItemText, MenuItem, Typography } from "@mui/material";
 
 export const EmptyCart = () => {
   return (
-    <MenuItem sx={{ p: 7, textAlign: "center" }}>
+    <MenuItem sx={{ p: 7, textAlign: "center", justifySelf: "center" }}>
       <ListItemText>
-        <Typography sx={{ fontWeight: 700, color: "neutral.darkGrayishBlue" }}>
+        <Typography
+          sx={{
+            fontWeight: 700,
+            color: "neutral.darkGrayishBlue",
+          }}
+        >
           Your Cart is empty.
         </Typography>
       </ListItemText>
