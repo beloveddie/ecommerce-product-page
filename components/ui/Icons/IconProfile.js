18b38b0d66bd0b82168fd772aa6bb40c58beba30
Eddiebee <edbeeotudor@gmail.com>
2022-10-24 12:22:53 +0100

    Add hover effect to profile image

--- components/ui/Icons/IconProfile.js
+++ components/ui/Icons/IconProfile.js
@@ -4,7 +4,13 @@ export const IconProfile = () => {
   return (
     <Tooltip title="Esteemed, Remy Sharp">
       <IconButton sx={{ p: 0 }}>
-        <Avatar alt="Remy Sharp" src="/images/image-avatar.png"></Avatar>
+        <Avatar
+          alt="Remy Sharp"
+          src="/images/image-avatar.png"
+          sx={{
+            "&:hover": { border: "2px solid", borderColor: "primary.main" },
+          }}
+        ></Avatar>
       </IconButton>
     </Tooltip>
   );
