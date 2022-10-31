2c9ddbebcd90c6a21f85bba13a94072b0df15b47
Eddiebee <edbeeotudor@gmail.com>
2022-10-31 12:46:54 +0100

    Hide some Components for the now

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import Header from "../components/Header/Header";
 import { MobileImageSlider } from "../components/ui/MobileImageSlider/MobileImageSlider";
 import { ProductDetail } from "../components/ui/ProductDetail";
 import { Cart } from "../components/Cart";
+import DesktopMain from "../components/ui/DesktopMain";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Cart />
+      {/* <Cart />
       <MobileImageSlider />
-      <ProductDetail />
+      <ProductDetail /> */}
+      <DesktopMain />
     </>
   );
 }
