d0ef0e5ddfe42e5bbb37bf95fb28daade70fd677
Eddiebee <edbeeotudor@gmail.com>
2022-11-01 15:42:19 +0100

    Refactor: Update CustomImage to meet UI needs

--- components/CustomImage/index.js
+++ components/CustomImage/index.js
@@ -1,20 +1,29 @@
 import Image from "mui-image";
+import { Box } from "@mui/material";
 
-export const CustomImage = ({ src, alt, height, width }) => (
-  <Image
-    sx={{ borderRadius: "1rem" }}
-    src={src}
-    height={height}
-    width={width}
-    fit="cover"
-    duration={3000}
-    easing="cubic-bezier(0.7, 0, 0.6, 1)"
-    showLoading
-    errorIcon
-    shift="top"
-    distance="100px"
-    shiftDuration={900}
-    bgColor="inherit"
-    alt={alt}
-  />
+export const CustomImage = ({ src, alt, height, width, sx }) => (
+  <Box
+    sx={{
+      width,
+      height,
+      ...sx,
+    }}
+  >
+    <Image
+      sx={{ borderRadius: "1rem" }}
+      src={src}
+      height={height}
+      width={width}
+      fit="cover"
+      duration={3000}
+      easing="cubic-bezier(0.7, 0, 0.6, 1)"
+      showLoading
+      errorIcon
+      shift="top"
+      distance="100px"
+      shiftDuration={900}
+      bgColor="inherit"
+      alt={alt}
+    />
+  </Box>
 );
